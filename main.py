742f5abd5c06f128429706cc5f297ec42074d8ec
zhangmengdan1 <2037228815@qq.com>
2025-09-09 18:13:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# 在这个文件下编写代码，题目具体要求见README.md文件
+#weight = input("请输入带有符号的重量")
+if weight[-2:] in ["kg"]:
+    poung = (eval(weight[0:-2])) * 2.2046
+    print("对应的重量为：{:.3f}磅".format(poung))
+elif weight[-2:] in ["pd"]:
+    kg = (eval(weight[0:-2])) / 2.2046
+    print("对应的重量为：{:.3f}千克".format(kg))
+else:
+    print("输入错误") 
