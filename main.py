989d4a97099c3a07beb5bbb77ed970ac13914021
zhangmengdan1 <2037228815@qq.com>
2025-09-12 10:59:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-#weight = input()
+weight = input()
 if weight[-2:] in ["kg"]:
     poung = (eval(weight[0:-2])) * 2.2046
-    print("对应的重量为：{:.3f}磅".format(poung))
+    print("对应的英制重量为：{:.3f}磅".format(poung))
 elif weight[-2:] in ["pd"]:
     kg = (eval(weight[0:-2])) * 0.4535
-    print("对应的重量为：{:.3f}千克".format(kg))
+    print("对应的公制重量为：{:.3f}公斤".format(kg))
 else:
     print("输入错误") 
